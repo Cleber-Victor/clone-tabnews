18836e75fc01785e7e9c9e6f2c08e479437857d4
Cleber-victor <cvsj@cin.ufpe.br>
2026-02-03 12:09:05 -0300

    add reference

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-  return <h1>29 Dá força ao cansado, e multiplica as forças ao que não tem nenhum vigor. 30 Os jovens se cansarão e se fatigarão, e os moços certamente cairão. 31 Mas os que esperam no Senhor renovarão as forças, subirão com asas como águias; correrão, e não se cansarão; caminharão, e não desfalecerão</h1>
+  return <h1>29 Dá força ao cansado, e multiplica as forças ao que não tem nenhum vigor. 30 Os jovens se cansarão e se fatigarão, e os moços certamente cairão. 31 Mas os que esperam no Senhor renovarão as forças, subirão com asas como águias; correrão, e não se cansarão; caminharão, e não desfalecerão.(Isaías 40:29-31)</h1>
 }
 
 export default Home;
